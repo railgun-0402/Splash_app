d31b66daf900751a65031e6af036f06c1c27889e
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-06-18 15:25:39 +0900

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Search.css';
+
+const Search = () => {
+    
+    return (
+        <form>
+            <input
+            className='Search'
+            />
+        </form>
+    );
+};
+export default Search;
